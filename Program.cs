07f0c964c402bb7ea74570d67ebe2e7272e9cdda
«Nikolay <pshenichnikovnf@mail.ru>
2022-09-06 01:02:35 +0300

    Метод сортировки массива

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@
         Console.Write(array[i] + " ");
     }
 }
+
+void SortingArray (string [] array){
+    
+     string[] Newarray = new string[array.Length];
+
+     for (int i = 0; i < array.Length; i++){      
+        if(array[i].Length <= 3){
+        array[i] = array[i];
+        Console.Write(array[i] + " "); 
+        }
+     }
+}
